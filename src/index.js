1933fc751dd6cee3a13dfb76dda2249cc55b4183
renzoafable <lbafable@up.edu.ph>
2018-07-02 17:44:18 +0800

    Edit CORS Middleware

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const session = require('express-session');
 const store = require('express-mysql-session');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const cors = require('cors');
 const db = require('./database');
 const router = require('./router');
 
@@ -16,20 +17,24 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // CORS
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', '*');
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, DELETE, GET');
-    return res.status(200).json({});
-  }
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', '*');
+//   if (req.method === 'OPTIONS') {
+//     res.header('Access-Control-Allow-Methods', 'PUT, POST, DELETE, GET');
+//     return res.status(200).json({});
+//   }
+//   next();
+// });
+app.use(cors({
+  origin: 'http://localhost:4200',
+  credentials: true
+}))
 
 // session store
 app.use(session({
-  key: 'fsrmgtsys',
-  secret: 'fsrmgtsys',
+  key: 'flirtly',
+  secret: 'flirtly',
   resave: true,
   saveUninitialized: true,
   store: sessionStore,
@@ -39,24 +44,6 @@ app.use(session({
 }));
 
 app.use('/api', router);
-
-// page error handling
-app.use((req, res, next) => {
-  const error = new Error('Page not found');
-  error.status = 404;
-  next(error);
-});
-
-// error catching
-app.use((error, req, res, next) => {
-  res.status(error.status || 500);
-  res.json({
-    error: {
-      status: error.status || 500,
-      message: error.message,
-    }
-  });
-});
 
 app.use('*', (req, res) => res.redirect('/'));
 
