19e9a4b2dab01c34b6797a5b3688e58d3158ca5e
renzoafable <lbafable@up.edu.ph>
2018-07-13 15:25:30 +0800

    Refactor middlewares

--- src/entities/connections/router.js
+++ src/entities/connections/router.js
@@ -2,15 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const db = require('../../database/index');
+const middlewares = require('../middlewares/middleware')();
 
 const authRepo = require('../authentication/repo')(db)
 const connectionsRepo = require('./repo')(db);
 const connectionsCtrl = require('./controller')(connectionsRepo, authRepo);
 
-const isSameUser = require('../middlewares/middleware').isSameUser;
-
 router.get('/', connectionsCtrl.getConnectionsOfUser);
-router.post('/send/:userID', isSameUser, connectionsCtrl.requestConnection);
+router.post('/send/:userID', middlewares.isSameUser, connectionsCtrl.requestConnection);
 router.put('/approve/:userID', connectionsCtrl.approveReceivedConnections);
 router.get('/sent', connectionsCtrl.getSentConnections);
 router.get('/received', connectionsCtrl.getReceivedConnections);
