7617518f41c7fc40068d4d79f54ffa649455260f
renzoafable <lbafable@up.edu.ph>
2018-07-13 15:25:30 +0800

    Refactor middlewares

--- src/entities/middlewares/middleware.js
+++ src/entities/middlewares/middleware.js
@@ -1,24 +1,31 @@
-exports.isLoggedIn = function(req, res, next) {
-  if (req.session.user) {
-    return next();
+const middlewares = function () {
+  const mw = {
+    isLoggedIn: (req, res, next) => {
+      if (req.session.user) {
+        return next();
+      }
+
+      res.status(401).json({
+        status: 401,
+        message: 'You must be logged in'
+      });
+    },
+
+    isSameUser: (req, res, next) => {
+      const { user } = req.session;
+      req.params.userID = parseInt(req.params.userID);
+      if (user.userID !== req.params.userID) {
+        return next();
+      }
+
+      res.status(403).json({
+        status: 403,
+        message: 'Cannot perform action with user'
+      });
+    }
   }
-  
-  res.status(401).json({
-    status: 401,
-    message: 'You must be logged in'
-  });
+
+  return mw;
 }
 
-exports.isSameUser = function(req, res, next) {
-  const { user } = req.session;
-  req.params.userID = parseInt(req.params.userID);
-  if (user.userID !== req.params.userID) {
-    return next();
-  }
-
-  res.status(403).json({
-    status: 403,
-    message: 'Cannot perform action with user'
-  });
-}
-
+module.exports = middlewares;
--- src/router.js
+++ src/router.js
@@ -2,14 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 // middlewares
-const middlewares = require('./entities/middlewares/middleware');
-const isLoggedIn = middlewares.isLoggedIn;
+const middlewares = require('./entities/middlewares/middleware')();
 
 const authRouter = require('./entities/authentication/router');
 const userRouter = require('./entities/users/router');
 
 router.use(authRouter);
-router.use(isLoggedIn);
+router.use(middlewares.isLoggedIn);
 router.use('/user', userRouter);
 
 module.exports = router;
