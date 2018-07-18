48223868a7895b34271a6cc686899a4fd0cbba29
renzoafable <lbafable@up.edu.ph>
2018-07-18 15:51:37 +0800

    Add sockets to send request and cancel request

--- src/index.js
+++ src/index.js
@@ -1,52 +1,79 @@
-const express = require('express');
-const session = require('express-session');
-const store = require('express-mysql-session');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const cors = require('cors');
-const db = require('./database');
-const router = require('./router');
+const express = require("express");
+const session = require("express-session");
+const store = require("express-mysql-session");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const cors = require("cors");
+const db = require("./database");
+const router = require("./router");
 
 const MySQLStore = store(session);
 const sessionStore = new MySQLStore({}, db);
 
 const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 
-io.on('connection', socket => {
-  console.log('User connected');
-  socket.on('message', (message) => {
+io.on("connection", socket => {
+  console.log("User connected");
+
+  socket.on("message", message => {
     socket.broadcast.emit(`message/${message.senderID}`, message);
     socket.broadcast.emit(`message/${message.receiverID}`, message);
+  });
+
+  socket.on("sendRequest", request => {
+    socket.broadcast.emit(`sendRequest/${request.userID}`, {
+      connectionID: request.connectionID,
+      firstName: request.connectionFirstName,
+      lastName: request.connectionLastName
+    });
+    socket.broadcast.emit(`receiveSentRequest/${request.connectionID}`, {
+      userID: request.userID,
+      firstName: request.userFirstName,
+      lastName: request.userLastName
+    });
+  });
+
+  socket.on("cancelRequest", request => {
+    console.log(request);
+    socket.broadcast.emit(`cancelRequest/${request.userID}`, request);
+    socket.broadcast.emit(
+      `receiveCancelledRequest/${request.connectionID}`,
+      request
+    );
   });
 });
 
 // middlewares
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(cors({
-  origin: 'http://localhost:4200',
-  credentials: true
-}))
+app.use(
+  cors({
+    origin: "http://localhost:4200",
+    credentials: true
+  })
+);
 
 // session store
-app.use(session({
-  key: 'flirtly',
-  secret: 'flirtly',
-  resave: true,
-  saveUninitialized: true,
-  store: sessionStore,
-  createDatabaseTable: true,
-  checkExpirationInterval: 9000000,
-  expiration: 86400000,
-}));
+app.use(
+  session({
+    key: "flirtly",
+    secret: "flirtly",
+    resave: true,
+    saveUninitialized: true,
+    store: sessionStore,
+    createDatabaseTable: true,
+    checkExpirationInterval: 9000000,
+    expiration: 86400000
+  })
+);
 
-app.use('/api', router);
+app.use("/api", router);
 
-app.use('*', (req, res) => res.redirect('/'));
+app.use("*", (req, res) => res.redirect("/"));
 
 // listen to port
 const port = process.env.PORT || 3001;
